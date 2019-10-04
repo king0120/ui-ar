302fbe6d241944d0732b546fedbd787f74d2e54f
king0120 <king0120@gmail.com>
2019-10-04 19:24:48 -0400

    Getting rid of static assets

--- src/app/main/pages/auth/reset-password/ResetPasswordPage.js
+++ src/app/main/pages/auth/reset-password/ResetPasswordPage.js
@@ -52,7 +52,7 @@ function ResetPasswordPage()
 
                         <CardContent className="flex flex-col items-center justify-center p-32">
 
-                            <img className="w-128 m-32" src="assets/images/logos/fuse.svg" alt="logo"/>
+
 
                             <Typography variant="h6" className="mt-16 mb-32">RESET YOUR PASSWORD</Typography>
 

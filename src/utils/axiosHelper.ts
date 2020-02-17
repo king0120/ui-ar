3e8233ac3d8b5dd735e529e8d6d58adae78719e2
king0120 <king0120@gmail.com>
2020-02-17 00:10:51 -0500

    update url

--- src/utils/axiosHelper.ts
+++ src/utils/axiosHelper.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const arAxios = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://app.auditionrevolution.com/' : '/',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://www.auditionrevolution.com/' : '/',
 });
 
 const accessToken = localStorage.getItem('accessToken');
